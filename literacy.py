40d9dfe60694b8b01730b720f3da133a4a066c66
Will Turner <rwturner17@gmail.com>
2023-03-19 18:55:40 -0400

    run literacy on itself

--- literacy.py
+++ literacy.py
@@ -7,8 +7,7 @@ import subprocess
 import ast
 import sys
 
-
-import subprocess
+from pathlib import Path
 
 import openai
 import logging
@@ -23,14 +22,18 @@ openai.api_key = OPENAI_API_KEY
 
 
 def generate_docstring(function_signature):
-    prompt = f"""I want you to write a docstring for a Python function.
-        If the function is very simple, a one-line docstring is fine.
-        If the function is not very simple, please include lines for the arguments and return values.
-        If the function is complicated, please include examples of inputs and outputs.
-        Limit the length of lines to 80 characters or less.
-        Please return just the docstring, surrounded by triple quotes.
-        Here is the code for the function:
-        {function_signature}"""
+    """Generate a docstring for a Python function using OpenAI's GPT-3 natural language processing model.
+
+Args:
+    function_signature (str): The signature of the function for which to generate a docstring.
+
+Returns:
+    str: The generated docstring.
+
+Example:
+    >>> generate_docstring('def add(x: int, y: int) -> int:')
+    'Add two integers together and return the result.'"""
+    prompt = Path("prompt.txt").read_text() + function_signature
     logger.debug(prompt)
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -49,6 +52,68 @@ def generate_docstring(function_signature):
 
 
 def process_file(filename):
+    """Process a Python file and add missing docstrings to its functions.
+
+Args:
+    filename (str): The path to the Python file to process.
+
+Returns:
+    None
+
+The function reads the contents of the file, parses it with the `ast` module,
+and extracts all the function definitions that lack a docstring. It then generates
+a docstring for each of these functions by analyzing their source code, and adds
+the docstring to the function definition. Finally, the modified file content is
+written back to the original file.
+
+Example:
+    Given the following Python file:
+
+    ```
+    def square(x):
+        return x ** 2
+
+    def cube(x):
+        
+        Compute the cube of a number.
+
+        Args:
+            x (int): The number to compute the cube of.
+
+        Returns:
+            int: The cube of the input number.
+        
+        return x ** 3
+    ```
+
+    Calling `process_file(my_file.py)` will modify the file to:
+
+    ```
+    def square(x):
+        
+        Compute the square of a number.
+
+        Args:
+            x (int): The number to compute the square of.
+
+        Returns:
+            int: The square of the input number.
+        
+        return x ** 2
+
+    def cube(x):
+        
+        Compute the cube of a number.
+
+        Args:
+            x (int): The number to compute the cube of.
+
+        Returns:
+            int: The cube of the input number.
+        
+        return x ** 3
+    ```
+"""
     logger.debug("Processing %s", filename)
     with open(filename, "r") as file:
         content = file.read()
@@ -72,6 +137,20 @@ def process_file(filename):
 
 
 def scan_codebase(directory):
+    """
+Scan a directory for Python files and process them if they are not ignored by Git.
+
+Args:
+    directory (str): The path to the directory to scan.
+
+Returns:
+    None.
+
+Examples:
+    >>> scan_codebase(/path/to/codebase)
+    # Scans the codebase directory for Python files and processes them if they are
+    # not ignored by Git.
+"""
     git_root = find_git_root(directory)
     gitignore_path = os.path.join(git_root, ".gitignore")
     logger.debug("GITIGNORE: %s", gitignore_path)
@@ -84,6 +163,18 @@ def scan_codebase(directory):
 
 
 def find_git_root(path):
+    """Find the root directory of a Git repository given a path.
+
+Args:
+    path (str): The starting directory to search for the Git root.
+
+Returns:
+    str: The absolute path of the Git root directory if found, None otherwise.
+
+Example:
+    >>> find_git_root('/home/user/project/subdir')
+    '/home/user/project'
+"""
     try:
         output = subprocess.check_output(
             ["git", "rev-parse", "--show-toplevel"], cwd=path
@@ -97,5 +188,5 @@ def find_git_root(path):
 
 
 if __name__ == "__main__":
-    codebase_directory = "/home/will/code/wolverine"
+    codebase_directory = "/home/will/code/literacy"
     scan_codebase(codebase_directory)
