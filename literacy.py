0910d2d4a1b8bf5012f036b866cee4ab46984d4a
Will Turner <rwturner17@gmail.com>
2023-03-20 20:47:13 -0400

    count the cost

--- literacy.py
+++ literacy.py
@@ -23,7 +23,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from display import FileStatusDisplay
 import ast
 import sys
-
+from typing import Tuple
 from pathlib import Path
 
 import openai
@@ -37,8 +37,13 @@ logger = logging.getLogger(__name__)
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "api-key")
 openai.api_key = OPENAI_API_KEY
 
+MODEL_NAME = "gpt-3.5-turbo"
+TOKEN_COST = 0.002 / 1000  # dollars per token
 
-def generate_docstring(function_name: str, function_signature: str):
+
+def generate_docstring(
+    function_name: str, function_signature: str
+) -> Tuple[str, float]:
     """
     Generate a docstring for a Python function using OpenAI's GPT-3 natural language processing model.
 
@@ -47,6 +52,7 @@ def generate_docstring(function_name: str, function_signature: str):
 
     Returns:
         str: The generated docstring.
+        float: The cost of the query in dollars.
 
     Example:
         >>> generate_docstring('def add(x: int, y: int) -> int:')
@@ -64,19 +70,21 @@ def generate_docstring(function_name: str, function_signature: str):
     )
     logger.debug(response)
     response_text = response.choices[0].message.content.strip()
+    n_tokens = response.usage.total_tokens
+    cost = n_tokens * TOKEN_COST
     logger.debug(response_text)
     sys.stdout.flush()
-    return response_text
+    return response_text, cost
 
 
-def process_file(filename):
+def process_file(filename) -> float:
     """Process a Python file and add missing docstrings to its functions.
 
     Args:
         filename (str): The path to the Python file to process.
 
     Returns:
-        None
+        float: The cost in dollars of the queries to the OpenAI API.
 
     The function reads the contents of the file, parses it with the `ast` module,
     and extracts all the function definitions that lack a docstring. It then generates
@@ -146,17 +154,15 @@ def process_file(filename):
     def update_function(function, content):
         function_source = ast.get_source_segment(content, function)
         old_signature = f"def {function.name}({ast.unparse(function.args)}):"
-        docstring = generate_docstring(function.name, function_source).replace('"', "")
+        docstring, cost = generate_docstring(function.name, function_source)
+        docstring = docstring.replace('"', "")
         new_signature = f'{old_signature}\n    """{docstring}"""'
-        return (
-            function.name,
-            old_signature,
-            new_signature,
-        )
+        return (function.name, old_signature, new_signature, cost)
 
     # Display function names in yellow
     display = FileStatusDisplay(filename, [function.name for function in functions])
     display.display()
+    total_cost = 0
 
     with ThreadPoolExecutor() as executor:
         futures = [
@@ -165,15 +171,18 @@ def process_file(filename):
         ]
 
         for future in as_completed(futures):
-            function_name, old_signature, new_signature = future.result()
+            function_name, old_signature, new_signature, cost = future.result()
             if old_signature and new_signature:
                 result = result.replace(old_signature, new_signature)
 
                 display.update(function_name, "green")
+                total_cost += cost
 
     display.finish()
+    logger.info("File cost: $%.4f", total_cost)
     with open(filename, "w") as file:
         file.write(result)
+    return total_cost
 
 
 def scan_codebase(directory):
@@ -189,11 +198,14 @@ def scan_codebase(directory):
     gitignore_path = os.path.join(git_root, ".gitignore")
     logger.debug("GITIGNORE: %s", gitignore_path)
     matches_gitignore = parse_gitignore(gitignore_path)
+    codebase_cost = 0
     for root, _, files in os.walk(directory):
         for file in files:
             file_path = os.path.join(root, file)
             if file.endswith(".py") and not matches_gitignore(file_path):
-                process_file(file_path)
+                file_cost = process_file(file_path)
+                codebase_cost += file_cost
+    logger.info("Codebase cost: $%.4f", codebase_cost)
 
 
 def find_git_root(path):
